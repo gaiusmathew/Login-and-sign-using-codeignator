1f48a9ab0d66c10b6bc38529abd43be33bdc341e
Gaius Mathew <gaiusmathew1996@gmail.com>
2017-11-20 16:05:04 +0530

    codeignator/session

---
+++ application/views/login_form.php
@@ -0,0 +1,58 @@
+<html>
+<?php
+if (isset($this->session->userdata['logged_in'])) {
+
+header("location: http://localhost/login/index.php/login/user_login_process");
+}
+?>
+<head>
+<title>Login Form</title>
+ <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
+</head>
+<body style="text-align: center;">
+<?php
+if (isset($logout_message)) {
+echo "<div class='message'>";
+echo $logout_message;
+echo "</div>";
+}
+?>
+<?php
+if (isset($message_display)) {
+echo "<div class='message'>";
+echo $message_display;
+echo "</div>";
+}
+?>
+<div id="main">
+<div id="login">
+<h2>Login Form</h2>
+<hr/>
+<?php echo form_open('login/user_login_process'); ?>
+<?php
+echo "<div class='error_msg'>";
+if (isset($error_message)) {
+echo $error_message;
+}
+echo validation_errors();
+echo "</div>";
+?>
+<label>UserName :</label>
+<input type="text" name="username" id="name" placeholder="username"/><br /><br />
+<label>Password :</label>
+<input type="password" name="password" id="password" placeholder="**********"/><br/><br />
+<input type="submit" value=" Login " name="submit"/><br />
+<a href="<?php echo base_url() ?>index.php/login/user_registration_show">To SignUp Click Here</a>
+<?php echo form_close(); ?>
+</div>
+</div>
+</body>
+</html>
